f482b7e30dd9b5d2b265776af3287e0960c3c15d
colefiscus <>
2021-02-17 23:14:15 -0700

    Add changes

--- src/Components/Movie/Movie.js
+++ src/Components/Movie/Movie.js
@@ -7,7 +7,7 @@ const Movie = ({ film, removeSpaces, matchMovie }) => {
     return (
         <Link to={`/movie/${removeSpaces(film.title)}`} onClick={() => matchMovie(film.title)} className="movieInfo">
             <section>
-                <p className="movieTitle">{film.title}</p>
+                <p className="movieLabel">{film.title}</p>
                 <p className="movieRating">{film.average_rating.toFixed(1)}</p>
             </section>
         </Link>
--- src/Views/MovieDetails/MovieDetails.js
+++ src/Views/MovieDetails/MovieDetails.js
@@ -4,16 +4,31 @@ import { Link } from 'react-router-dom';
 
 const MovieDetails = ({ currentMovie, isLoading }) => {
 
-  console.log(isLoading)
-
   if (isLoading) {
     return (
-      <h1>Loading...</h1>
+      <h1 className="loading">Loading...</h1>
     )
   } else {
     console.log(currentMovie)
     return (
-      <h1>{currentMovie.movie.title}</h1>
+      <section className="movieDetails">
+        <div className="movieTitle">
+          <h1>{currentMovie.movie.title}</h1>
+          <h2>{currentMovie.movie.tagline}</h2>
+        </div>
+        <div className="movieOverview">
+          <article className="movieSpecifics">
+            <p><b>Overview:</b><br/>{currentMovie.movie.overview}</p>
+            <p><b>Release Date: </b>{currentMovie.movie.release_date}</p>
+            <p><b>Genre(s):</b> {currentMovie.movie.genres}</p>
+            <p><b>Stars:</b> {currentMovie.movie.average_rating.toFixed(1)} / 10</p>
+            <p><b>Runtime:</b> {currentMovie.movie.runtime} minutes</p>
+            <p><b>Budget:</b> ${currentMovie.movie.budget}</p>
+            <p><b>Revenue:</b> ${currentMovie.movie.revenue}</p>
+          </article>  
+          <img src={currentMovie.movie.poster_path}/>
+        </div>
+      </section>
     )
   }
 }
