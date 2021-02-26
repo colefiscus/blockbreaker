befbe6ad973915aa9052c70dd44274f33db66e11
colefiscus <>
2021-02-25 20:22:17 -0700

    Edit styling and add randomize trivia answers func

--- src/Components/TriviaQuestion/TriviaQuestion.js
+++ src/Components/TriviaQuestion/TriviaQuestion.js
@@ -3,15 +3,36 @@ import './TriviaQuestion.css';
 
 const TriviaQuestion = ({ trivia }) => {
 
+  const decodeQuestion = (question) => {
+    const text = document.createElement("textarea");
+    text.innerHTML = question;
+    return text.value;
+  }
+
+  const randomizeQuestions = () => {
+    let allAnswers = [...trivia.incorrect_answers, trivia.correct_answer]
+    let currentIndex = allAnswers.length, tempAnswer, randomIndex
+    while(0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      tempAnswer = allAnswers[currentIndex];
+      allAnswers[currentIndex] = allAnswers[randomIndex];
+      allAnswers[randomIndex] = tempAnswer;
+    }
+    return allAnswers
+  }
+
+  const randomizedAnswers = randomizeQuestions()
+
   return (
     <section className="triviaQuestion">
       <h2><u>MOVIE TRIVIA</u></h2>
-      <p>{trivia.question}</p>
+      <p>{decodeQuestion(trivia.question)}</p>
       <form className="triviaAnswers">
-        <input type="button" value={trivia.incorrect_answers[0]} />
-        <input type="button" value={trivia.incorrect_answers[1]} />
-        <input type="button" value={trivia.incorrect_answers[2]} />
-        <input type="button" value={trivia.correct_answer} />
+        <input type="button" value={randomizedAnswers[0]} />
+        <input type="button" value={randomizedAnswers[1]} />
+        <input type="button" value={randomizedAnswers[2]} />
+        <input type="button" value={randomizedAnswers[3]} />
       </form>
     </section>
   )
