b808b4da352344a90f14c0b7d84fe882fe195432
colefiscus <coletfiscus@gmail.com>
2021-06-21 16:33:23 -0600

    Add structure to Error component

--- src/Views/Error/Error.js
+++ src/Views/Error/Error.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Error.css';
 
 const Error = () => {
   return (
-
+    <div className='errorPage'>
+      <h2>This page cannot be found. Please check spelling in URL or click below to be directed to home page.</h2>
+      <Link to='/home' className='errorHomeLink'>HOME</Link>
+    </div>
   )
 }
 
