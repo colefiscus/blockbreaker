194afba2b7df1c3b9f921a9b8ea49096062336a7
colefiscus <coletfiscus@gmail.com>
2021-06-22 16:27:18 -0600

    Change Header styling

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './Header.css';
-import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <header>
-            <Link className="header" to="/home">
-                BLOCKBREAKER
-            </Link>
+        <header className="header">
+            BLOCKBREAKER
         </header>
     )
 }
