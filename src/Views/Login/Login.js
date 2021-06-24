edbfddd274210caa65a211f8a44b70e2304a21c1
colefiscus <coletfiscus@gmail.com>
2021-06-24 15:38:32 -0600

    Edit Login structure

--- src/Views/Login/Login.js
+++ src/Views/Login/Login.js
@@ -2,21 +2,15 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './Login.css';
 
-class Login extends Component {
-    constructor() {
-        super()
-    }
-
-    render() {
-        return (
-            <div className="loginGreeting">
-                <h1>WELCOME TO<br></br>BLOCKBREAKER</h1>
-                <Link to="/home">
-                    <button className="enterButton">ENTER</button>
-                </Link>
-            </div>
-        )
-    }
+const Login= () => {
+    return (
+        <div className="loginGreeting">
+            <h1>WELCOME TO<br></br>BLOCKBREAKER</h1>
+            <Link to="/home">
+                <button className="enterButton">ENTER</button>
+            </Link>
+        </div>
+    )
 }
 
 export default Login;
